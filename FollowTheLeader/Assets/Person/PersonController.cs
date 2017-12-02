81864020439cc787272a3fc5956204d7295a167d
jeroenpx <jeroenpx@users.noreply.github.com>
2017-12-02 14:45:59 +0100

    Changes to person controller (jump)

--- FollowTheLeader/Assets/Person/PersonController.cs
+++ FollowTheLeader/Assets/Person/PersonController.cs
@@ -9,21 +9,28 @@ public class PersonController : MonoBehaviour {
 	private float speed = 1f;
 
 	[SerializeField]
-	private float inairtime;
+	private float inairtime = 1f;
 
 	[SerializeField]
-	private float gravity = 10f;
+	private float jumpheight = 1f;
 
 	// What to do next frame?
 	private Vector2 moveDir;
+	private bool jump;
+	private float jumpStartTime=-100f;
 
 	// Private components
 	private Rigidbody2D rigidBody;
 	private Transform child;
+	private Vector3 childDelta;
+	private bool jumping;
+
+	public float t;
 
 	void Awake() {
 		rigidBody = GetComponent<Rigidbody2D> ();
 		child = transform.Find ("Graphics");
+		childDelta = child.localPosition;
 	}
 
 	/**
@@ -36,13 +43,34 @@ public class PersonController : MonoBehaviour {
 	/**
 	 * MESSAGE: Jump!
 	 */
+	[ContextMenu("Jump")]
 	public void Jump() {
-		// TODO
+		jump = true;
 	}
 
 	void FixedUpdate() {
 		// Currently just set the velocity
 		rigidBody.velocity = moveDir*speed;
+
+		if (jump) {
+			if (jumping && jumpStartTime < Time.time - inairtime) {
+				// end of jump
+				jump = false;
+				jumping = false;
+			} else {
+				if (!jumping) {
+					// start jumping
+					jumping = true;
+					jumpStartTime = Time.time;
+				}
+			}
+			if (jumping) {
+				// we are in the air!
+				t = (Time.time-jumpStartTime)/inairtime;
+				float tsquared = t*t;
+				child.localPosition = childDelta + new Vector3(0f, -4*jumpheight*tsquared+4*jumpheight*t);
+			}
+		}
 
 		// Reset move dir
 		moveDir = new Vector2 ();
