249fd4f6f0624d76b12603d04fd1c7d1bee31684
Bart Huylebroeck <bart.huylebroeck@pandora.be>
2017-12-02 15:29:38 +0100

    special start square

--- FollowTheLeader/Assets/Scripts/LevelBuilder.cs
+++ FollowTheLeader/Assets/Scripts/LevelBuilder.cs
@@ -3,15 +3,17 @@ using UnityEngine;
 
 public class LevelBuilder : MonoBehaviour {
 
+
+    public SquareBehaviour connectSquare;
     public SquareBehaviour[] prefabs;
     private Queue<SquareBehaviour> levels;
-    private SquareBehaviour lastSquare;
 
     private void Start()
     {
-        lastSquare = Instantiate<SquareBehaviour>(prefabs[Random.Range(0, prefabs.Length)]);
-        lastSquare.transform.position = Vector3.zero;
-        levels.Enqueue(lastSquare);
+        levels = new Queue<SquareBehaviour>();
+        //connectSquare = Instantiate<SquareBehaviour>(prefabs[Random.Range(0, prefabs.Length)]);
+        //connectSquare.transform.position = Vector3.zero;
+        levels.Enqueue(connectSquare);
     }
 
     [ContextMenu("AddSquare")]
@@ -23,8 +25,9 @@ public class LevelBuilder : MonoBehaviour {
     private SquareBehaviour GenerateSquare()
     {
         SquareBehaviour result = Instantiate<SquareBehaviour>(prefabs[Random.Range(0, prefabs.Length)]);
-        result.LinkToPrevious(lastSquare);
-        lastSquare = result;
+        result.transform.SetParent(this.transform);
+        result.LinkToPrevious(connectSquare);
+        connectSquare = result;
         return result;
     }
 }
--- FollowTheLeader/Assets/Scripts/SquareBehaviour.cs
+++ FollowTheLeader/Assets/Scripts/SquareBehaviour.cs
@@ -4,10 +4,15 @@ public class SquareBehaviour : MonoBehaviour {
 
     public Transform entrance;
     public Transform exit;
+    private int nr;
+    private static int serial = 0;
+
+    public delegate void CommanderEnteredHandler(Commander c);
+    public event CommanderEnteredHandler CommanderEnteredEvent;
 
 	// Use this for initialization
 	void Start () {
-		
+        this.nr = serial++;
 	}
 	
 	// Update is called once per frame
@@ -15,6 +20,7 @@ public class SquareBehaviour : MonoBehaviour {
 		
 	}
 
+    //to be called by level builder to make sure each new square connects to the previous
     public void LinkToPrevious(SquareBehaviour prev)
     {
         // we need to match our entrance with the previous exit:
@@ -23,4 +29,12 @@ public class SquareBehaviour : MonoBehaviour {
         //now move the whole square
         this.transform.position += move;
     }
+
+    //to be called by level builder when the hero has advanced far enough. Will trigger destruction animation
+    public void Dissolve()
+    {
+
+    }
+
+
 }
